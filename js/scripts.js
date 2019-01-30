013b896e5e393a224492d00daf32b4710ffac3a8
Dave Sarbora and Jimmy Zebroski <jimmy@>
2019-01-30 13:01:05 -0800

    combine dice object with player object and add prototypes for rolling, updating score, and changing player

--- js/scripts.js
+++ js/scripts.js
@@ -1,19 +1,47 @@
-function Player(name, score){
+function PlayerList(players){
+  this.players = [],
+  this.currentID = 0,
+  this.currentTurn= 1
+};
+
+PlayerList.prototype.addPlayer = function(player){
+  player.id = this.assignID();
+  this.players.push(player);
+  this.currentPlayer = this.players[0]
+};
+
+PlayerList.prototype.assignID = function(){
+  this.currentID += 1;
+  return this.currentID;
+}
+
+PlayerList.prototype.changePlayer = function(){
+  this.currentTurn += 1
+  if(this.currentTurn % 2 != 0){
+    this.currentPlayer = this.players[0]
+  }
+  else{
+    this.currentPlayer = this.players[1]
+  }
+};
+
+PlayerList.prototype.scoreUpdate = function(){
+  this.currentPlayer.updateScore();
+  this.changePlayer();
+}
+
+/*        Player Logic      */
+function Player(name, score, currentRoll){
   this.name = name,
-  this.score = 0
-};
-
-Player.prototype.updateScore =function(){
-  var scoreIncrease = 0
-  scoreIncrease += dice.tallyUp()
-};
-
-
-function Dice (currentRoll){
+  this.score = 0,
   this.currentRoll = []
 };
 
-Dice.prototype.roll = function(){
+Player.prototype.updateScore =function(){
+  this.score += this.tallyUp()
+};
+
+Player.prototype.roll = function(){
   var thisRoll = (Math.floor(Math.random() * (6)) + 1);
   if (thisRoll > 1){
   console.log(thisRoll);
@@ -25,14 +53,19 @@ else{
 }
 };
 
-Dice.prototype.tallyUp = function(){
+Player.prototype.tallyUp = function(){
   var number = 0;
-  for(var i = 0; i<dice.currentRoll.length; i++){
-    number += dice.currentRoll[i];
+  for(var i = 0; i<this.currentRoll.length; i++){
+    number += this.currentRoll[i];
   };
-  console.log(number);
-  dice.currentRoll = []
+  return number;
+  this.currentRoll = [];
 };
 
-var dice = new Dice();
-var player = new Player("Dave");
+
+/*     User Interface Logic     */
+var playerList = new PlayerList();
+var player = new Player("Player1");
+var player2 = new Player("Player2");
+playerList.addPlayer(player);
+playerList.addPlayer(player2);
