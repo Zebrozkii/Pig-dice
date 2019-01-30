9422c2243bb69113d88c6f4f19b749458bfd286f
Dave Sarbora and Jimmy Zebroski <jimmy@>
2019-01-30 14:36:02 -0800

    finished javascript objects and prototypes

--- js/scripts.js
+++ js/scripts.js
@@ -16,9 +16,12 @@ PlayerList.prototype.assignID = function(){
 }
 
 PlayerList.prototype.changePlayer = function(){
+  $("#arrow1").toggle();
+  $("#arrow2").toggle();
   this.currentTurn += 1
   if(this.currentTurn % 2 != 0){
-    this.currentPlayer = this.players[0]
+    this.currentPlayer = this.players[0];
+
   }
   else{
     this.currentPlayer = this.players[1]
@@ -39,6 +42,7 @@ function Player(name, score, currentRoll){
 
 Player.prototype.updateScore =function(){
   this.score += this.tallyUp()
+  this.currentRoll = [];
 };
 
 Player.prototype.roll = function(){
@@ -50,6 +54,7 @@ Player.prototype.roll = function(){
 else{
   console.log(thisRoll);
   this.currentRoll = [];
+  playerList.changePlayer();
 }
 };
 
@@ -59,7 +64,7 @@ Player.prototype.tallyUp = function(){
     number += this.currentRoll[i];
   };
   return number;
-  this.currentRoll = [];
+
 };
 
 
@@ -69,3 +74,45 @@ var player = new Player("Player1");
 var player2 = new Player("Player2");
 playerList.addPlayer(player);
 playerList.addPlayer(player2);
+
+function scoresReset(){
+  $("#currentRoll").text(0);
+  playerList.players[0].currentRoll= [];
+  playerList.players[1].currentRoll = [];
+  playerList.players[0].score = 0;
+  playerList.players[1].score = 0;
+  $("#playerOneScore").text(playerList.players[0].score);
+  $("#playerTwoScore").text(playerList.players[1].score);
+  playerList.currentTurn = 0;
+  playerList.currentPlayer = playerList.players[0];
+  $("h2").hide();
+  $("#arrow1").show();
+  $("#arrow2").hide();
+};
+
+$(function(){
+  $("#roll").click(function(){
+    playerList.currentPlayer.roll();
+    $("h2").hide()
+    $("#currentRoll").text(playerList.currentPlayer.tallyUp());
+    $("#playerOneScore").text(playerList.players[0].score);
+    $("#playerTwoScore").text(playerList.players[1].score);
+  }); //end roll.click function
+
+  $("#hold").click(function(){
+    $("#currentRoll").text(0);
+    playerList.scoreUpdate();
+    $("#playerOneScore").text(playerList.players[0].score);
+    $("#playerTwoScore").text(playerList.players[1].score);
+      if(playerList.players[0].score >= 100){
+        $("#playerOneCongrats").show();
+      }
+      else if(playerList.players[1].score >= 100){
+        $("#playerTwoCongrats").show();
+      }
+  });  //end hold.click function
+
+  $("#reset").click(function(){
+    scoresReset();
+  }); //end reset click function
+}); //end document ready function
